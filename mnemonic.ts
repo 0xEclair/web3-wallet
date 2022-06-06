64879765687243c1be46d5c892352bf567746d80
lumiere-eclair <38656355+eclair-lumiere@users.noreply.github.com>
2022-06-06 08:00:00 +0800

    :art: format the code

--- mnemonic.ts
+++ mnemonic.ts
@@ -1,17 +1,17 @@
-import * as bip39 from 'bip39';
+import * as bip39 from 'bip39'
 
 export const generateMnemonic = (): string => {
-  return bip39.generateMnemonic();
-};
+  return bip39.generateMnemonic()
+}
 
 export const mnemonicToSeed = (mnemonic: string): string => {
-  return bip39.mnemonicToSeedSync(mnemonic).toString('hex');
-};
+  return bip39.mnemonicToSeedSync(mnemonic).toString('hex')
+}
 
 const mnemonicToEntropy = (mnemonic: string): string => {
-  return bip39.mnemonicToEntropy(mnemonic);
-};
+  return bip39.mnemonicToEntropy(mnemonic)
+}
 
 export const validateMnemonic = (mnemonic: string): boolean => {
-  return bip39.validateMnemonic(mnemonic);
-};
+  return bip39.validateMnemonic(mnemonic)
+}
